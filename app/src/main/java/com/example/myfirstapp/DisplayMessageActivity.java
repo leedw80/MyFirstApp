db4d090cd504851767218e8ae01dd9da8cb3e8af
slippers <slippers2016@naver.com>
2017-09-15 10:18:17 +0900

    20170915

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,11 +1,15 @@
 package com.example.myfirstapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,8 +22,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String count = intent.getStringExtra(MainActivity.EXTRA_COUNT);
 
-        TextView textView = (TextView) findViewById(R.id.activity_display_message);
-        textView.setText(message);
+        ImageView imageView = (ImageView) findViewById(R.id.img_display);
+
+
+        if (Integer.parseInt(count) >=  10 ) {
+            imageView.setImageResource(R.mipmap.stampgo100);
+        }else if(Integer.parseInt(count) >= 8 && Integer.parseInt(count) < 10 ) {
+            imageView.setImageResource(R.mipmap.stampgo80);
+        }else if(Integer.parseInt(count) >= 5 && Integer.parseInt(count) < 8 ) {
+            imageView.setImageResource(R.mipmap.stampgo50);
+        }else {
+            imageView.setImageResource(R.mipmap.stampgo20);
+        }
+
+        TextView textView1 = (TextView) findViewById(R.id.activity_display_message);
+        textView1.setText(message);
+
+        Button button = (Button) findViewById(R.id.btn_count);
+        button.setText(count);
+
+        if (Integer.parseInt(count) > 10) {
+            button.setEnabled(true);
+        } else {
+            button.setEnabled(false);
+        }
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,39 +1,175 @@
 package com.example.myfirstapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ScrollingTabContainerView;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String EXTRA_COUNT = "com.example.myfirstapp.COUNT";
 
     StringBuilder stringBuilder;
+
+    DBHelper dbHelper;
+    TextView result;
+    String fDate;
+
+    String message = "";
+    String count = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //////////////////////////////////////////////////////////////////////////////////////
+        Button send = (Button) findViewById(R.id.btn_send);
+        send.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("dwlee", "onClick:");
+                if (stringBuilder.length() == 13) {
+                    TextView textView = (TextView) findViewById(R.id.edit_message);
+                    message = textView.getText().toString();
+
+                    int price = 123;
+
+                    dbHelper.insert(fDate, message, price);
+                    count = dbHelper.getResult(message);
+                    result.setText(count);
+
+                    intent.putExtra(EXTRA_MESSAGE, message);
+                    intent.putExtra(EXTRA_COUNT, count);
+                    startActivity(intent);
+                } else {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();     //닫기
+                        }
+                    });
+                    alert.setMessage("Please again.");
+                    alert.show();
+                }
+            }
+        });
+
+
+        Button search = (Button) findViewById(R.id.btn_search);
+        search.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (stringBuilder.length() == 13) {
+                    TextView textView = (TextView) findViewById(R.id.edit_message);
+                    message = textView.getText().toString();
+                    count = dbHelper.getResult(message);
+                    result.setText(count);
+
+                    intent.putExtra(EXTRA_MESSAGE, message);
+                    intent.putExtra(EXTRA_COUNT, count);
+                    startActivity(intent);
+                } else {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();     //닫기
+                        }
+                    });
+                    alert.setMessage("Please again.");
+                    alert.show();
+                }
+            }
+        });
+        //////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+        String sdcardPath = null;
+        String sdcardStat = Environment.getExternalStorageState();
+        if(sdcardStat.equals(Environment.MEDIA_MOUNTED))
+        {
+            sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        }
+
+        Log.e("dwlee","sdcardPath:"+sdcardPath);
+        */
+
+        dbHelper = new DBHelper(getApplicationContext(), "stampgo.db", null, 1);
+
+        // 테이블에 있는 모든 데이터 출력
+        result = (TextView) findViewById(R.id.txt_result);
+        result.setMovementMethod((ScrollingMovementMethod.getInstance()));
+        // 날짜는 현재 날짜로 고정
+        // 현재 시간 구하기
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+
+        // 출력될 포맷 설정
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        fDate = simpleDateFormat.format(date);
+
         stringBuilder = new StringBuilder();
 
         TextView textView = (TextView) findViewById(R.id.edit_message);
         textView.setText(stringBuilder.append("010-").toString());
     }
-
+    /*
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-        TextView textView = (TextView) findViewById(R.id.edit_message);
-        String message = textView.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        Log.e("dwlee","sendMessage:");
+        if (stringBuilder.length() == 13) {
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            TextView textView = (TextView) findViewById(R.id.edit_message);
+            String message = textView.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();     //닫기
+                }
+            });
+            alert.setMessage("Please again.");
+            alert.show();
+        }
     }
+
+    public void searchMessage(View view) {
+        if (stringBuilder.length() == 13) {
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            TextView textView = (TextView) findViewById(R.id.edit_message);
+            message = textView.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();     //닫기
+                }
+            });
+            alert.setMessage("Please again.");
+            alert.show();
+        }
+    }
+    */
 
     public void num7(View view) {
         TextView textView = (TextView) findViewById(R.id.edit_message);
